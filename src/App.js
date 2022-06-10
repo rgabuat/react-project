d6b7fc1c375213dda77c182c9d5824551d5a07a0
rgabuat <johncarlgabuat@gmail.com>
2022-06-11 05:52:00 +0800

    first react commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ApplicationForm from './components/ApplicationForm'
+
+function App() {
+  return (
+    <div className="App">
+      <ApplicationForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ApplicationForm.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Grid, Paper,Container,Typography,TextField,InputLabel,Input,FormHelperText,FormControl,Card,CardContent,Button } from '@mui/material' //import 
+
+const ApplicationForm=()=>{
+    return(
+
+        <Card>
+            <CardContent>
+                <Grid container spacing={1}>
+                    <Grid xs={12} sm={6} item>
+                        <TextField size="small" label="First name" placeholder="Enter first name" variant="outlined" fullWidth />
+                    </Grid>
+                    <Grid xs={12} sm={6} item>
+                        <TextField size="small" label="Last name" placeholder="Enter last name" variant="outlined" fullWidth />
+                    </Grid>
+                    <Grid xs={12} item>
+                        <TextField size="small" type="email" label="Email" placeholder="Enter email" variant="outlined" fullWidth />
+                    </Grid>
+                    <Grid xs={12} item>
+                        <TextField size="small" type="number" label="Phone" placeholder="Enter phone number" variant="outlined" fullWidth />
+                    </Grid>
+                    <Grid xs={12} item>
+                        <TextField size="small"  label="Message" multiline rows={4} placeholder="Enter message..." variant="outlined" fullWidth />
+                    </Grid>
+                    <Grid xs={12} item>
+                        <Button variant="contained" color="primary" fullWidth>SUBMIT</Button>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+
+    )
+}
+
+export default ApplicationForm
